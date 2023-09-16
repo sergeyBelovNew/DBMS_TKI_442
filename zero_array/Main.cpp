277b0cf31892d3bfd7c2f031b261d8fbfe03b3fb
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2023-09-17 05:48:44 +0800

    fix all(except input_array)

--- zero_array/Main.cpp
+++ zero_array/Main.cpp
@@ -1,116 +1,102 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
-
 
 /*----- Prototyping procedures -----*/
 
-
 /**
- * \brief Create array.
- * \param size - size of your array
- * \return array(fill).
+ * @brief Create array.
+ * @param size - size of your array
+ * @return array(fill).
 */
-
 
 int* input_array(const size_t size);
 
-
 /**
- * \brief Simple bubble sort(2 param).
- * \param array - your "raw" array
- * \param size - size of your array
+ * @brief Simple bubble sort(2 param).
+ * @param array - your "raw" array
+ * @param size - size of your array
 */
-
 
 void sort_bubble(int* array, const size_t size);
 
-
 /**
- * \brief Find max element of array(2 param).
- * \param array - your "raw" array
- * \param size - size of your array
- * \return max element of array.
+ * @brief Find max element of array(2 param).
+ * @param array - your "raw" array
+ * @param size - size of your array
+ * @return max element of array.
 */
-
 
 int get_max_element_of_array(int* array, const size_t size);
 
-
 /**
- * \brief Find min element of array(2 param).
- * \param array - your "raw" array
- * \param size - size of your array
- * \return min element of array.
+ * @brief Find min element of array(2 param).
+ * @param array - your "raw" array
+ * @param size - size of your array
+ * @return min element of array.
 */
-
 
 int get_min_element_of_array(int* array, const size_t size);
 
-
 /**
- * \brief Output of array in index - array value format.
- * \param array - your "raw" array
- * \param size - size of your array
+ * @brief Output of array in index - array value format.
+ * @param array - your "raw" array
+ * @param size - size of your array
 */
 
-
-void show_array_index_to_value(int* array, const size_t size);
-
+string to_string_array(int* array, const size_t size);
 
 /**
- * \brief for see output of sorting, find max, min elements of array and custom func for output array.
+ * @brief for see output of sorting, find max, min elements of array and custom func for output array.
 */
-
 
 int main() {
-	size_t size;
+	int size;
 
 	cout << "Input array size ";
 	cin >> size;
 
 	if (size <= 0) {
 		cout << "You entered wrong size!\n";
-		return 1;
+		throw std::out_of_range(" Error!");
 	}
 
 	int* example_array = new int[size];
 
 	example_array = input_array(size);
 
-	show_array_index_to_value(example_array, size);
+	cout << to_string_array(example_array, size) << "\n";
 
-	get_max_element_of_array(example_array, size);
+	cout << "Max element: " << get_max_element_of_array(example_array, size) << "\n";
 
-	get_min_element_of_array(example_array, size);
+	cout << "Min element: " << get_min_element_of_array(example_array, size) << "\n";
 
 	sort_bubble(example_array, size);
 
-	show_array_index_to_value(example_array, size);
+	cout << to_string_array(example_array, size) << "\n";
 
-	delete[] example_array;
+	if (example_array != nullptr) {
+		delete[] example_array;
+		example_array = nullptr;
+	}
+
 	return 0;
 }
 
-
 /* ----- Realization procedures ----- */
-
 
 int* input_array(const size_t size) {
 	int* array = new int[size];
 	cout << "Input array data(int type)\n";
-	for (size_t index = 0; index < size; index++) {
+	for (size_t index = 0; index < size; index++)
 		cin >> array[index];
-	}
 	return array;
 }
 
-
 void sort_bubble(int* array, const size_t size) {
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = i + 1; j < size; j++) {
+	for (size_t i = 0; i < size - 1; i++) {
+		for (size_t j = i + 1; j < size; j++) {
 			if (array[i] > array[j]) {
 				int temp = array[i];
 				array[i] = array[j];
@@ -118,38 +104,29 @@ void sort_bubble(int* array, const size_t size) {
 			}
 		}
 	}
-	cout << "Bubble sort complite";
 }
-
 
 int get_max_element_of_array(int* array, const size_t capability) {
 	int max_element = array[0];
-	for (int i = 0; i < capability - 1; i++) {
-		if (array[i] < array[i + 1]) {
+	for (size_t i = 0; i < capability - 1; i++) {
+		if (array[i] < array[i + 1])
 			max_element = array[i + 1];
-		}
 	}
-	cout << "Finding max element complite\
-             \nCurrent max element:" << max_element << "\n";
 	return max_element;
 }
 
-
 int get_min_element_of_array(int* array, const size_t size) {
 	int min_element = array[0];
-	for (int i = 0; i < size - 1; i++) {
+	for (size_t i = 0; i < size - 1; i++) {
 		if (array[i + 1] < array[i])
 			min_element = array[i + 1];
 		}
-	cout << "Finding min element complite\
-             \nCurrent min element:" << min_element << "\n";
 	return min_element;
 }
 
-
-void show_array_index_to_value(int* array, const size_t size) {
-	for (int i = 0; i < size; i++) {
-		cout << "Element [" << to_string(i) << "] - value: " \
-			 << to_string(array[i]) << "\n";
-	}
+string to_string_array(int* array, const size_t size) {
+	string out;
+	for (size_t i = 0; i < size; i++) 
+		out += to_string(array[i]);
+	return out;
 }
